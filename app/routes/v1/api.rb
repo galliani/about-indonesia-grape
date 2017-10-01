fd486e1b9ca6235ace1112834eac287621bb670e
Galih Muhammad <galih0muhammad@gmail.com>
2017-10-01 15:53:56 +0700

    refactor the province endpoints into separate file for tidyness

--- app/routes/v1/api.rb
+++ app/routes/v1/api.rb
@@ -4,51 +4,7 @@ module Routes
       version 'v1'
       format :json
 
-      namespace :provinces do
-        desc 'Return list of provinces.'
-        get '/' do
-          Province.all
-        end
-
-        desc 'Return a province.'
-        params do
-          requires :id, type: Integer, desc: 'Province id.'
-        end
-        route_param :id do
-          get do
-            Province.find(params[:id])
-          end
-        end
-
-        desc 'Create a province.'
-        params do
-          requires :province, type: Hash do
-            requires :name, type: String, desc: 'Your Province.'
-          end          
-        end
-        post do
-          Province.create!(params[:province])
-        end
-
-        desc 'Update a Province.'
-        params do
-          requires :id, type: String, desc: 'Province ID.'
-          requires :province, type: Hash do
-            requires :name, type: String, desc: 'Your Province.'
-          end
-        end
-        put ':id' do
-          Province.find(params[:id]).update(params[:province])
-        end
-
-        desc 'Delete a Province.'
-        params do
-          requires :id, type: String, desc: 'Province ID.'
-        end
-        delete ':id' do
-          Province.find(params[:id]).destroy
-        end
-      end
+      include Routes::V1::Provinces
 
     end
   end
